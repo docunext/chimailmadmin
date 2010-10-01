9b64a15012a2b83e050b4bc9c8ceb8a2f61c9f4f
Docunext <albert.lash@docunext.com>
2010-10-01 01:01:33 -0400

    mini

--- chimailmadmin.rb
+++ chimailmadmin.rb
@@ -1,6 +1,6 @@
 ###
 # Program:: http://www.docunext.com/wiki/Sinatra
-# Component:: notapp.rb
+# Component:: chimailmadmin.rb
 # Copyright:: Savonix Corporation
 # Author:: Albert L. Lash, IV
 # License:: Gnu Affero Public License version 3
